805795f465b4eaf1671c4cc9e37432b156d36dfb
Tawfeeqoh <tawfeeqohh@gmail.com>
2025-09-13 19:16:53 +0100

    Add files via upload

---
+++ User.js
@@ -0,0 +1,25 @@
+// src/models/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  role: { type: String, enum: ["freelancer", "employer"], required: true },
+  rating: { type: Number, default: 0 },
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Method to check password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
